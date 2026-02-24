22ec880612f115c0b80f434a00ebf79f49700fe9
TueznX <101438471+TueznX@users.noreply.github.com>
2026-02-24 20:45:53 -0300

    Delete scripts directory

--- scripts/info1.js
+++
@@ -1,8 +0,0 @@
-function toggleMute() {
-  const video = document.getElementById('infoVideo');
-  const icon = document.getElementById('volumeIcon');
-
-  video.muted = !video.muted;
-  icon.classList.toggle('fa-volume-high', !video.muted);
-  icon.classList.toggle('fa-volume-xmark', video.muted);
-}
