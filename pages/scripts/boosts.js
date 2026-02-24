cede9aec2c389f4df0e3e4e33c0311bff84acc00
TueznX <101438471+TueznX@users.noreply.github.com>
2026-02-24 20:45:47 -0300

    Delete pages directory

--- pages/scripts/boosts.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.boosts__card').forEach(card => {
-  card.addEventListener('click', () => {
-    const id = card.dataset.id;
-    window.location.href = `product-details.html?id=${id}`;
-  });
-});
